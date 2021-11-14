b9a183cc6a8558bc49aa970dc33e7edd48e30923
Arthur <arthur.cunha@acct.global>
2021-11-14 19:51:12 -0300

    finish mobile

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -4,51 +4,23 @@ import right from "../../img/right.svg"
 
 export const Pagination = () => {
     return (
-        <div className="row pagination__custom justify-content-center">
-            <nav aria-label="Page navigation example">
-                <ul className="pagination d-flex align-items-center">
-                    <li className="page-item">
-                        <a className="page-link" href="/" aria-label="Previous">
-                            <img src={left} alt="left arrow icon" />
-                        </a>
-                    </li>
-                    <li className="page-item">
-                        <a className="page-link" href="/">1</a>
-                    </li>
-                    <li className="page-item">
-                        <a className="page-link" href="/">2</a>
-                    </li>
-                    <li className="page-item">
-                        <a className="page-link" href="/">3</a>
-                    </li>
-                    <li className="page-item">
-                        <a className="page-link" href="/">4</a>
-                    </li>
-                    <li className="page-item">
-                        <a className="page-link" href="/">5</a>
-                    </li>
-                    <li className="page-item active">
-                        <a className="page-link" href="/">6</a>
-                    </li>
-                    <li className="page-item">
-                        <a className="page-link" href="/">7</a>
-                    </li>
-                    <li className="page-item">
-                        <a className="page-link" href="/">8</a>
-                    </li>
-                    <li className="page-item">
-                        <a className="page-link" href="/">9</a>
-                    </li>
-                    <li className="page-item">
-                        <a className="page-link" href="/">10</a>
-                    </li>
-                    <li className="page-item">
-                        <a className="page-link" href="/" aria-label="Next">
-                            <img src={right} alt="right awwor icon" />
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+        <div className="row pagination__custom">
+            <div className="d-flex align-items-center justify-content-center">
+                <img src={left} alt="left arrow icon" />
+                <div className="d-flex align-items-center justify-content-center">
+                    <p>1</p>
+                    <p>2</p>
+                    <p>3</p>
+                    <p>4</p>
+                    <p>5</p>
+                    <span>6</span>
+                    <p>7</p>
+                    <p>8</p>
+                    <p>9</p>
+                    <p>10</p>
+                </div>
+                <img src={right} alt="right arrow icon" />
+            </div>
 
         </div>
     )
